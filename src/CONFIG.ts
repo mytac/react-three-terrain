aa5df2f3245a59cbbb39443f128b9183cafc15fb
mytac <my_tac@126.com>
2024-05-08 14:43:32 +0800

    chore(config): 配置文件

---
+++ src/CONFIG.ts
@@ -0,0 +1,10 @@
+/** 地块矩阵维度*/
+export const GRID_MATRIX_LENGTH = [9, 12]
+/** 单位数*/
+export const UNIT = 5
+
+/** 直方图宽度*/
+export const GRAM_WIDTH = 0.15
+
+/** 镜头定位 */
+export const CAMERA_POSITION = [15, 10, 15]
