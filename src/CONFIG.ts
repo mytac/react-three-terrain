9e5d295b3bd7d7174094bcbe5a3e30549237971f
mytac <my_tac@126.com>
2024-05-09 18:43:41 +0800

    chore: 新增可配置字段：直方图、文字显隐；灯光强度；

--- src/CONFIG.ts
+++ src/CONFIG.ts
@@ -2,13 +2,23 @@
 /** 单位大小*/
 export const UNIT = 2
 /** 地块矩阵维度*/
-export const GRID_MATRIX_LENGTH = [4, 4] // x,y
+export const GRID_MATRIX_LENGTH = [9, 9] // x,y
+/** 打开wireframe */
+// export const WIREFRAME = true
+export const WIREFRAME = false
+
+export const SEGMENT = 128
+// export const TEXTURE_IMAGE = 'round1.png'
+export const TEXTURE_IMAGE = 'image5.jpg'
 
 /* --------------直方图--------------------- */
 /** 直方图宽度*/
 export const GRAM_WIDTH = 0.15
+export const SHOW_GRAM = true
 
 /* --------------文字--------------------- */
+/** 是否显示文字 */
+export const SHOW_TITLE = true
 /** 文字高度  */
 export const TEXT_HEIGHT = 2
 /** 文字大小  */
@@ -23,3 +33,32 @@ export const OFFSET_UNIT = 1
 /*---------------镜头---------------------*/
 /** 镜头定位 */
 export const CAMERA_POSITION = [8, 15, 8]
+
+/* --------------灯光--------------------- */
+export const POINT_LIGHT_INTENSITY = 250
+export const PONIT_LIGHT_HEIGHT = 10
+
+/** shader */
+export const XSHADER_VERTEXT = `
+uniform sampler2D heightMap;
+			uniform float heightRatio;
+			varying vec2 vUv;
+			varying float hValue;
+			varying vec3 cl;
+			void main() {
+			    vUv = uv;
+			    vec3 pos = position;
+		        cl = texture2D(heightMap, vUv).rgb;
+		        hValue = texture2D(heightMap, vUv).r;
+		        pos.y = hValue * heightRatio;
+		        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos,1.0);
+		    }`
+
+export const XSHADER_FRAGMENT = `
+varying float hValue;
+			varying vec3 cl;
+			void main() {
+		 		float v = abs(hValue - 1.);
+		 		gl_FragColor = vec4(cl, .8 - v * v) ; 
+		    }
+`
