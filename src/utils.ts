ec77fcfc614f23d7a16a45cdd33adbb92400d9a9
mytac <my_tac@126.com>
2024-05-10 18:10:58 +0800

    fix(utils): 修复两个工具函数逻辑

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import * as CONFIG from './CONFIG'
 
-const { OFFSET_UNIT, UNIT, GRID_MATRIX_LENGTH, TEXT_HEIGHT } = CONFIG
+const { OFFSET_UNIT, UNIT, GRID_MATRIX_LENGTH, TEXT_HEIGHT, ADJACENT_RADIUS } =
+  CONFIG
 
 /** 边缘检测 */
 const isEdge = ([x, y, z]) => {
@@ -19,7 +20,7 @@ type Position = [number, number, number]
 export const getGridTextOffset: (p: Position) => Position = (source) => {
   const [a, b] = GRID_MATRIX_LENGTH
   let xoffset = 0,
-    zoffset = 0
+    yoffset = 0
   const [x, y, z] = source
   if (x - OFFSET_UNIT < 0) {
     // 左边缘
@@ -28,41 +29,41 @@ export const getGridTextOffset: (p: Position) => Position = (source) => {
     xoffset += OFFSET_UNIT
   }
 
-  if (z - OFFSET_UNIT < 0) {
-    zoffset -= OFFSET_UNIT
-  } else if (z + OFFSET_UNIT > b) {
-    zoffset += OFFSET_UNIT
+  if (y - OFFSET_UNIT < 0) {
+    yoffset -= OFFSET_UNIT
+  } else if (y + OFFSET_UNIT > b) {
+    yoffset += OFFSET_UNIT
   }
 
-  return [x + xoffset, TEXT_HEIGHT, z + zoffset]
+  return [x + xoffset, y + yoffset, TEXT_HEIGHT]
 }
 
 /** 获取n个最高点坐标（除去半径为RADIUS的临近点） */
-export const filterMaxZWithoutAdjacentPoint = (
-  arr: [number, number, number][],
-  sum: number
-) => {
-  const RADIUS = 1
+export const filterMaxZWithoutAdjacentPoint = (position, sum: number) => {
   const result = []
-  const sorted = [...arr]
+  const count = position.count
+  const posArray = []
+  for (let i = 0; i < count; i++) {
+    posArray.push([position.getX(i), position.getY(i), position.getZ(i)])
+  }
+  const sorted = [...posArray]
   sorted.sort((a, b) => {
     const [x1, y1, z1] = a
     const [x2, y2, z2] = b
-    return z1 - z2
+    return z2 - z1
   })
-
   for (let i = 0; i < sorted.length; i++) {
     const p = sorted[i]
     const [x, y, z] = p
-    if (result.length > sum) return result
+    if (result.length >= sum) return result
     if (i == 0) {
       result.push(p)
     } else {
       const notValid = result.some(([x2, y2, z2]) => {
-        if ((x2 - x) ** 2 + (y2 - y) ** 2 <= RADIUS * 2) {
-          return false
+        if ((x2 - x) ** 2 + (y2 - y) ** 2 <= ADJACENT_RADIUS) {
+          return true
         }
-        return true
+        return false
       })
       if (!notValid) {
         result.push(p)
